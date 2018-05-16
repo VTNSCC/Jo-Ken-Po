5f30205cdc1085fe2e2239e090d7bb12b16ffac8
VTNSCC <37680210+VTNSCC@users.noreply.github.com>
2018-05-16 12:31:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function Myway() {
-    alert("Rules:\n YOU WILL PLAY AGAINST THE COMPUTER!\n - Rock X Scissors= Rock wins!\n - Scissors X Paper= Scissors wins!\n - Paper X Rock= Paper wins!\n - If you WIN you'll get 1 point\n - If it's a TIE nobody gets points\n - If the PC WIN's he'll get 1 point too  ")
+    alert("Rules:\n YOU WILL PLAY AGAINST THE COMPUTER!\n - Rock X Scissors= Rock wins!\n - Scissors X Paper= Scissors wins!\n - Paper X Rock= Paper wins!\n - If you WIN you'll get 1 point\n - If it's a TIE nobody gets points\n - If the PC WIN's he'll get 1 point too\n LET'S PLAY NOW!!!  ")
 }
 
 var pps = 0;
